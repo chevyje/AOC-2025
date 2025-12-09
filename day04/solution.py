a929162726bbe09c3bd84067830aa396e341567d
chevyje <jurre.blankers@gmail.com>
2025-12-09 14:43:29 +0100

    Day 4 Part 2 - Solved
    Needed to put the input in their again.

--- day04/solution.py
+++ day04/solution.py
@@ -1,57 +1,54 @@
 import os
 
 base = os.path.dirname(__file__)
-path = os.path.join(base, "test_input.txt")
+path = os.path.join(base, "input.txt")
 
 answer = 0
 
 def start():
     with open(path) as file:
-        lines = file.readlines()
-        total_lines: int = len(lines)
-        for index, line in enumerate(lines):
-            length = len(line.strip())
-            previous_line = None
-            next_line = None
-            if index - 1 >= 0:
-                previous_line = lines[index-1].strip()
-            if index + 1 <= total_lines - 1:
-                next_line = lines[index+1].strip()
-
-            check_rows([previous_line, line.strip(), next_line], length)
-            print(f"line {index}: {line} \n")
+        lines = [line.strip() for line in file.readlines()]
+        check_papers(lines)
     print(f"total answer: {answer}")
+
+def check_papers(lines):
+    new_lines = []
+    total_lines: int = len(lines)
+
+    for index, line in enumerate(lines):
+        length = len(line)
+        previous_line = lines[index - 1] if index - 1 >= 0 else None
+        next_line = lines[index + 1] if index + 1 < total_lines else None
+        new_lines.append(check_rows([previous_line, line, next_line], length))
+
+    if new_lines == lines:
+        return
+
+    check_papers(new_lines)
 
 def check_rows(lines: list[str | None], length: int):
     global answer
+    current_line = lines[1]
     for index in range(length):
-        current_line = lines[1].strip()
-        print(f"{index}: {current_line[index]}")
         if current_line[index] == ".":
             continue
 
-        begin = 0
-        end = length
-        if index - 1 > 0:
-            begin = index -1
-        if index + 1 < length:
-            end = index + 1
+        begin = max(0, index - 1)
+        end = min(length - 1, index + 1)
 
         count = -1
         for line in lines:
             count += check_line(line, begin, end)
-        print(f"amount of @ around: {count}")
         if count < 4:
+            current_line = current_line[:index] + "." + current_line[index + 1:]
             answer += 1
-            print(f"current answer: {answer} \n")
+    return current_line
 
 def check_line(line: str | None, begin: int, end: int) -> int:
     if line is None:
         return 0
     try:
-        line = line.strip()
         line = line[begin:end+1]
-        print(line)
         return line.count("@")
     except IndexError:
         return 0
